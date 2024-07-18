47e1f2b86f36f13f5d02dd47d9383086397cb849
Alexandr Green <alexandr.v3.41@gmail.com>
2024-07-19 00:43:36 +1000

    comment change

--- conung2Macros.py
+++ conung2Macros.py
@@ -5,7 +5,7 @@ import threading
 keyboard_controller = keyboard.Controller()
 mouse_controller = mouse.Controller()
 
-def heal_macro(): #Быстрое применение лечения через контекстное меню
+def heal_macro(): #quick application of healing through the context menu of the main character
     
     keyboard_controller.press(keyboard.Key.ctrl)
     time.sleep(0.2)  
@@ -20,7 +20,7 @@ def heal_macro(): #Быстрое применение лечения через
     mouse_controller.position = (216, 301)
     mouse_controller.click(mouse.Button.left, 1)
 
-def load_macro(): #Быстрая загрызка из топа списка сохранений
+def load_macro(): #fast loading from the top of the save list
     
     keyboard_controller.press(keyboard.Key.esc)
     keyboard_controller.release(keyboard.Key.esc)
@@ -34,7 +34,7 @@ def load_macro(): #Быстрая загрызка из топа списка с
     mouse_controller.click(mouse.Button.left, 1)
 
 double_click_in_progress = False
-def double_click(x, y): #Двойной клик при прокрутке коллесика мыши вверх
+def double_click(x, y): #double-click when scrolling the mouse wheel to speed up character movement
     global double_click_in_progress
     if not double_click_in_progress:
         double_click_in_progress = True
